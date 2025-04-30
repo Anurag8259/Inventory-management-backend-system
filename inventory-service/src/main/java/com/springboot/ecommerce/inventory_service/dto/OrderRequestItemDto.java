26ad623814c3b39eb86dc81a46148b884cefa263
Anurag8259 <anuragdas2002ad@gmail.com>
2025-04-30 12:38:05 +0530

    Setting up Open Feign -Part 2

--- inventory-service/src/main/java/com/springboot/ecommerce/inventory_service/dto/OrderRequestItemDto.java
+++ inventory-service/src/main/java/com/springboot/ecommerce/inventory_service/dto/OrderRequestItemDto.java
@@ -1,8 +1,10 @@
 package com.springboot.ecommerce.inventory_service.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class OrderRequestItemDto {
     private Long productId;
     private Integer quantity;
